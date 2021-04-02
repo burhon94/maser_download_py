33717cc3e855ba2598aff2f280260b48ca6dd8a3
burhon94 <tohirov1994@gmail.com>
2021-04-02 10:19:14 +0500

    feat: UI & formApp

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
-import requests
+from PyQt5 import QtWidgets
+from downloader import Ui_MainWindow
 
 
-def add_queue(url):
-    total_sections = 10
-    response = requests.head(url)
+class DownloadApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
-    if response.status_code > 299:
-        print('status Error')
-    else:
-        a = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-        size = response.headers["Content-Length"]
-        each_size = int(int(size) / total_sections)
-
-        for i in range(0, len(a)):
-            if i == 0:
-                a[i][0] = 0
-            else:
-                a[i][0] = a[i - 1][1] + 1
-
-            if i < 10 - 1:
-                a[i][1] = a[i][0] + each_size
-            else:
-                a[i][1] = int(size) - 1
-        print(a)
+        h = 332
+        w = 388
+        self.setFixedSize(w, h)
 
 
-add_queue("http://tiktok.mix.tj/video/7/46/6034ec9eca6a4.mp4")
+app = QtWidgets.QApplication([])
+window_main = DownloadApp()
+window_main.show()
+app.exec()
